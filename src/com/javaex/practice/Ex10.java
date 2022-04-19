76c03beb5fd6988497a1e79e767b62300d683b49
som2plestory <som2plestory@naver.com>
2022-04-19 10:32:56 +0900

    omission

---
+++ src/com/javaex/practice/Ex10.java
@@ -0,0 +1,29 @@
+package com.javaex.practice;
+
+
+import java.util.Scanner;
+
+public class Ex10 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하세요");
+		
+		int n = 0;
+		int i = 0;
+		while(i<5) {
+			System.out.print("숫자: ");
+			int n1 = sc.nextInt();
+			if(n>n1) {
+				n=n;
+			}else {
+				n=n1;
+			}
+			i++;
+		}	
+		System.out.println("최대값은: " + n +"입니다.");
+		
+		
+		sc.close();
+	}
+}
