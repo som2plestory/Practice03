ec5cb89ec72eac799ac12b46a67c9f858e6e01dc
som2plestory <som2plestory@naver.com>
2022-04-19 11:35:56 +0900

    2022-04-19 revise

--- src/com/javaex/practice/Ex10.java
+++ src/com/javaex/practice/Ex10.java
@@ -9,7 +9,7 @@ public class Ex10 {
 		
 		System.out.println("숫자를 입력하세요");
 		
-		int n = 0;
+/*		int n = 0;
 		int i = 0;
 		while(i<5) {
 			System.out.print("숫자: ");
@@ -24,6 +24,20 @@ public class Ex10 {
 		System.out.println("최대값은: " + n +"입니다.");
 		
 		
+		sc.close();*/
+		
+		
+		int n = 0;
+		for(int i=0; i<5; i++ ) {
+			System.out.print("숫자: ");
+			int n1 = sc.nextInt();
+			if(n>n1) {
+				n=n;
+			}else {
+				n=n1;
+			}
+		}
+		System.out.println("최대값은: " + n +"입니다.");
 		sc.close();
 	}
 }
